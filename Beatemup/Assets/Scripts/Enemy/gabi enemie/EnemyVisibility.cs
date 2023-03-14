8a782749e2d32da6ca25eab4050739a5986f27fc
OPISTO\gabriele.gargani <gabri.gargani@gmail.com>
2023-03-14 14:02:25 +0200

    flashfeedback when enemies hitted lifebar enemies

--- Beatemup/Assets/Scripts/Enemy/gabi enemie/EnemyVisibility.cs
+++ Beatemup/Assets/Scripts/Enemy/gabi enemie/EnemyVisibility.cs
@@ -13,27 +13,43 @@ public class EnemyVisibility : MonoBehaviour,IDamageable
     public LayerMask visibilityBlockers;
     public GameObject projectile;
     public GameObject firestart;
+    Material mat;
     NavMeshAgent enemy;
     Transform player;
+    public UIhealthbar healthbar;
+   
     public float maxSightRange = 15f;
     public float maxSightAngle = 45f;
     float timeToFire = 4f;
     float timeToSword = 2f;
     float cd;
-    int health =1;
+    int  maxHealth = 5;
+    int currentHealth;
+    float blinkIntensity = 10;
+    float blinkDuration = 0.1f;
+    float blinkTimer ;
 
     void Start() {
+        currentHealth =maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
         enemy = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        mat = GetComponent<Renderer>().material;
 
     }
 
     void Update() {
+        // blinking effect
+        blinkTimer -= Time.deltaTime;
+       float lerp = Mathf.Clamp01(blinkTimer / blinkDuration);
+        float intesity = (lerp * blinkIntensity) + 1;
+       mat.color = Color.white * intesity;
+        //skinmeshrender.material.color =...
 
 
         
       
-
+        // checking where the player is
         var origin = transform.position + 0.5f * Vector3.up;
         var targetPos = player.position + 0.5f * Vector3.up;
         var dir = targetPos - origin;
@@ -103,11 +119,14 @@ public class EnemyVisibility : MonoBehaviour,IDamageable
     }
   public void TakeDamage(HitData hit) {
 
-        health -= hit.damage;
-        Debug.Log("enemy attackked");
+
+        blinkTimer = blinkDuration; //reset timer
+        currentHealth -= hit.damage;
+        healthbar.SetHealth(currentHealth);
+        Debug.Log("enemy attacked");
         
        
-        if (health <= 0) {
+        if (currentHealth <= 0) {
 
             Die();
         }
@@ -122,18 +141,7 @@ public class EnemyVisibility : MonoBehaviour,IDamageable
             target.TakeDamage(new HitData(1,Vector3.back));
         }
     }
-    public void Hit(int damageTaken) {
-
-         health -= damageTaken;
-         if (health <= 0) {
-
-             Die();
-         }
-     }
-
-
-
-
+    
     void Die() {
 
         Destroy(gameObject, 3);
