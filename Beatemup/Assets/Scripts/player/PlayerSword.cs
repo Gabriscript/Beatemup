43277dbabc7302eecfdaeb9e392e2449f6badcff
OPISTO\gabriele.gargani <gabri.gargani@gmail.com>
2023-04-18 14:44:15 +0300

    knockback fixed

--- Beatemup/Assets/Scripts/player/PlayerSword.cs
+++ Beatemup/Assets/Scripts/player/PlayerSword.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerSword : MonoBehaviour {
 
-    Rigidbody rb;
-    // Start is called before the first frame update
+    Transform player;
+    public float knockbackPower = 500;
     void Start()
     {
-      
+        player = GetComponentInParent<PlayerMovementScript>().transform;
     }
 
     // Update is called once per frame
@@ -17,34 +17,31 @@ public class PlayerSword : MonoBehaviour {
         
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        var c = collision.collider.GetComponentInParent<IDamageable>();
-        c.TakeDamage(new HitData(1, Vector3.back));
-    }
+  
 
 
-    /* private void OnTriggerEnter(Collider collision) {
+   private void OnTriggerEnter(Collider collision) {
          var c = collision.GetComponentInParent<IDamageable>();
-         var origin = transform.position + 0.5f * Vector3.up;
+         var origin = player.position + 0.5f * Vector3.up;
          var targetPos = collision.transform.position + 0.5f * Vector3.up;
          var dir = targetPos - origin;
-        // dir = dir.normalized;
-       //  dir.y = 0;
+        dir = dir.normalized;
+       dir.y = 0;
 
 
-
+        
 
              if (c != null ) {
 
 
                  c.TakeDamage(new HitData(1, dir));
 
-             collision.GetComponentInParent<Rigidbody>().AddForce(Vector3.left ,ForceMode.Impulse);
+             collision.GetComponentInParent<Rigidbody>().AddForce(dir *knockbackPower);
 
 
 
 
 
          }
-     }*/
+     }
 }
