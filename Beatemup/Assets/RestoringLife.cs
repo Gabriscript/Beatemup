8b78416506bcae3328fb08d5fc04ba9eb3054ff5
OPISTO\gabriele.gargani <gabri.gargani@gmail.com>
2023-04-20 11:11:28 +0300

    healing partciles spawn in trigger area

--- Beatemup/Assets/RestoringLife.cs
+++ Beatemup/Assets/RestoringLife.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class RestoringLife : MonoBehaviour {
     PlayerMovementScript player;
+    public GameObject Healingsymbol;
+   
 
-    private void Start() {
+    private void Awake() {
         player = FindObjectOfType<PlayerMovementScript>();
+        Healingsymbol.SetActive(false);
     }
     private void OnTriggerStay(Collider other) {
         if (player.currentHealth < player.maxHealth)
             player.currentHealth++;
-            }
+        Healingsymbol.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player"))
+            Healingsymbol.SetActive(false);
+    }
+
+
+
+
+
 }
